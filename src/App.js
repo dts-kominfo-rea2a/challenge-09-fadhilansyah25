f8e43100aa60873db54de3669d7a189d577ecab2
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2022-06-17 22:18:30 +0700

    commit: challenge-09 REA2A done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-
+import Header from '../src/components/Header.js';
+import Contact from '../src/components/Contact'
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      {contacts.map((contact, i) => {
+        return <Contact key={i} data={contact} />
+      })}
     </div>
   );
 }
